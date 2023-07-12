15a3c454cfb18e453da403ab042809ae5e8eaf4a
Jude <gangdia@yahoo.com>
2023-07-12 11:10:25 -0600

    terraform code

---
+++ code1/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "demo" {
+  ami           = "ami-04823729c75214919"
+  count         = 1
+  instance_type = var.instance_type
+tags = {
+  Name = "Test-dev-server"
+  Terraform = "true"
+  Owner = "dev"
+  env = "dev"
+}
+}
---
+++ datasource/data.tf
@@ -0,0 +1,4 @@
+data "aws_instance" "remote-ec2" {
+  instance_id = "i-004e80c60285b484e"
+
+}
